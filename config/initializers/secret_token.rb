0eb732fb233ea0415d42413772cbaaccd3e8d807
Rayko <rayko.drg@gmail.com>
2011-08-19 20:55:39 -0300

    Rails 3 base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DistroaulasRails3::Application.config.secret_token = '10844a60e43cd908ab25b6b928ae7a611ed394ded6562077b198a360ad540a8f40a301b730a82b8c0b9be0b7c2643e16643f5e69575c520dabc0c0115b8f2ea2'
