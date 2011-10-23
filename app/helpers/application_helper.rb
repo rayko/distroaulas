c2ad259c7fcad531f9d266ae5452f396a3b751ef
Rayko <rayko.drg@gmail.com>
2011-10-23 03:39:08 -0300

    Design corrected, added some design to index views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
+  def show_icon
+    raw "<span title='Ver' class='ui-icon ui-icon-document'></span>"
+  end
 
+  def edit_icon
+    raw "<span title='Editar' class='ui-icon ui-icon-pencil'></span>"
+  end
+
+  def delete_icon
+    raw "<span title='Eliminar' class='ui-icon ui-icon-trash'></span>"
+  end
 end
