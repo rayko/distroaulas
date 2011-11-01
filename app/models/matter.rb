5790fc7e6d70fe18e9419f3d2196c5645bc1329b
Rayko <rayko.drg@gmail.com>
2011-11-01 00:51:40 -0300

    Improved matter view adding occurrences info on show

--- app/models/matter.rb
+++ app/models/matter.rb
@@ -5,4 +5,15 @@ class Matter < ActiveRecord::Base
   belongs_to :career
 
   has_many :events
+
+  def occurrences options={}
+    until_date = options[:before] || Date.today + 5.years
+    after_date = options[:after] || Date.new
+    occurrences = []
+    events = Event.find :all, :conditions => {:matter_id => self.id}
+    events.each do |event|
+      occurrences << event.to_rical.occurrences(:starting => after_date, :before => until_date)
+    end
+    return occurrences.flatten
+  end
 end
