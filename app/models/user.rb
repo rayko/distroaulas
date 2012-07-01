bafd6e78407e9112297d2ad2483895aafd95e410
Rayko <rayko.drg@gmail.com>
2012-07-01 04:02:43 -0300

    Login is now posible with either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,30 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :full_name, :role, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :full_name, :role, :email, :password, :password_confirmation, :remember_me, :login
+
+  validates_uniqueness_of :username
+  validates :username, :presence => true
+
+  attr_accessor :login
 
   before_validation :check_role
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def self.find_for_authentication(conditions)
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login }]).first
+  end
+
 
   # Posible roles for users
   def self.roles
