e6cfaea051f5ccd31f0370d55336582245e07e63
Rayko <rayko.drg@gmail.com>
2012-08-01 23:18:13 -0300

    Added a user on the seeds and a tech document

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+puts 'Creating first user...'
+User.create :email => 'admin@example.com', :username => 'admin', :full_name => 'Main Administrator', :role => 'admin', :password => 'superadmin', :password_confirmation => 'superadmin'
+
+puts 'First user created!'
+puts 'Mail: admin@example.com'
+puts 'Username: admin'
+puts 'Pass: superadmin'
+puts '---'
+puts 'Use the credentials avobe to access Distroaulas for the first time.'
+puts 'It\'s highly recommended to delete the first user or at least change the password for better security.'
